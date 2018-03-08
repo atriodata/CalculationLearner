c4d9d9f18bbcd5a7e4346c225ad7c77af6895fd8
Atrio <info@atrio.co.in>
2018-03-08 16:31:34 +0530

    Test Series Added

--- app/src/main/java/com/atrio/calculationlearner/CategoryActivity.kt
+++ app/src/main/java/com/atrio/calculationlearner/CategoryActivity.kt
@@ -1,11 +1,10 @@
 package com.atrio.calculationlearner
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
+import android.view.View
 import kotlinx.android.synthetic.main.activity_category.*
-import com.atrio.calculationlearner.animation.MyBounceInterpolator
 
 
 
@@ -15,12 +14,25 @@ class CategoryActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_category)
 
-         val myAnim = AnimationUtils.loadAnimation(this, R.anim.bounce) as Animation
-        val interpolator = MyBounceInterpolator(0.2, 20.0)
-        myAnim.interpolator = interpolator
-        btn_add.startAnimation(myAnim)
-        btn_sub.startAnimation(myAnim)
-        btn_mul.startAnimation(myAnim)
-        btn_div.startAnimation(myAnim)
+        btn_adds.setOnClickListener(View.OnClickListener {
+            intent=Intent(this@CategoryActivity,TestSeriesActivity::class.java)
+            intent.putExtra("categoryvalue","+")
+            startActivity(intent)
+        })
+        btn_subs.setOnClickListener(View.OnClickListener {
+            intent=Intent(this@CategoryActivity,TestSeriesActivity::class.java)
+            intent.putExtra("categoryvalue","-")
+            startActivity(intent)
+        })
+        btn_muls.setOnClickListener(View.OnClickListener {
+            intent=Intent(this@CategoryActivity,TestSeriesActivity::class.java)
+            intent.putExtra("categoryvalue","*")
+            startActivity(intent)
+        })
+        btn_divs.setOnClickListener(View.OnClickListener {
+            intent=Intent(this@CategoryActivity,TestSeriesActivity::class.java)
+            intent.putExtra("categoryvalue","/")
+            startActivity(intent)
+        })
     }
 }
---
+++ app/src/main/java/com/atrio/calculationlearner/model/NumData.kt
@@ -0,0 +1,10 @@
+package com.atrio.calculationlearner.model
+
+/**
+ * Created by Arpita Patel on 07-03-2018.
+ */
+data class NumData(val param1: String,
+                   val symbol:String,
+                   val param2: String,
+                   val equal:String,
+                   var numresult: String)
